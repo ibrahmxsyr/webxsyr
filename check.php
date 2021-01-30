4e76225c257d7100ac6b0901f6c8f7e7fa6c08e9
ibrahmxsyr <78240037+ibrahmxsyr@users.noreply.github.com>
2021-01-30 03:14:51 +0300

    Create check.php

---
+++ check.php
@@ -0,0 +1,19 @@
+{"status":"<?php
+
+$k1 = $_GET ["key"];
+
+switch ($k1) {
+    case "IOSGGAR74":
+    case "IOSGGAR":
+    case "ROYAL74":
+    case "ROYAL":
+    case "Ibrahem-Alnayef":
+
+
+    echo "true";
+        break;
+	default:
+		echo "false";
+		break;
+}
+?>","uuid":"<?php echo $_GET['udid']; ?>"}
