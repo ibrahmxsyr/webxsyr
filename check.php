7976c84fac762d69d6790375d8cde9b5e93b532c
ibrahmxsyr <78240037+ibrahmxsyr@users.noreply.github.com>
2021-01-30 03:18:02 +0300

    Update check.php

--- check.php
+++ check.php
@@ -3,13 +3,7 @@
 $k1 = $_GET ["key"];
 
 switch ($k1) {
-    case "IOSGGAR74":
     case "IOSGGAR":
-    case "ROYAL74":
-    case "ROYAL":
-    case "Ibrahem-Alnayef":
-
-
     echo "true";
         break;
 	default:
